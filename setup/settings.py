b60b3c8c93157169268cbd998f80a6ccb026ff85
Lucas Gervasoni <jaxellucas@gmail.com>
2024-07-22 11:19:09 -0300

    Adding templates , and postgresql to the project

--- setup/settings.py
+++ setup/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": str(os.getenv('PASSWORD')),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
