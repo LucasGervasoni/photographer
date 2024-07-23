0a87253070066ea14bccd59094a2691e593ee4e6
Lucas Gervasoni <jaxellucas@gmail.com>
2024-07-23 19:27:30 -0300

    Adding all the pages and styles

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import IndexView
+from .views import LoginView, ServicesView, ServicesCreateArtists, ServicesCreateOrders, ServicesListArtists,ServicesListOrders, UserPageOrders, UserUploadPage
 
 urlpatterns = [
-  path('', IndexView.as_view(), name='login'),
+  path('', LoginView.as_view(), name='login'),
+  path('services/', ServicesView.as_view(), name='services'),
+  path('services/create/artists', ServicesCreateArtists.as_view(), name='create_artists'),
+  path('services/create/orders', ServicesCreateOrders.as_view(), name='create_orders'),
+  path('services/list/artists', ServicesListArtists.as_view(), name='list__artists'),
+  path('services/list/orders', ServicesListOrders.as_view(), name='list__orders'),
+  path('orders/', UserPageOrders.as_view(), name='user__orders--page'),
+  path('upload/', UserUploadPage.as_view(), name='user__upload--page'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,26 @@
 # Create your views here.
 from django.views.generic import TemplateView
 
-class IndexView(TemplateView):
+class LoginView(TemplateView):
         template_name = "login_page.html"
+
+class ServicesView(TemplateView):
+        template_name = "admin/services.html"
+        
+class ServicesCreateArtists(TemplateView):
+        template_name = "admin/services__create--artists.html"
+
+class ServicesCreateOrders(TemplateView):
+        template_name = "admin/services__create--orders.html"
+
+class ServicesListArtists(TemplateView):
+        template_name = "admin/services__list--artists.html"
+
+class ServicesListOrders(TemplateView):
+        template_name = "admin/services__list--orders.html"
+
+class UserPageOrders(TemplateView):
+        template_name = "user-pages/user__orders--page.html"
+        
+class UserUploadPage(TemplateView):
+        template_name = "user-pages/user__upload--page.html"
