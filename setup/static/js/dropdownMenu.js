d304ab1f0484f2fa80a5fdabd4012d10096503f5
Lucas Gervasoni <jaxellucas@gmail.com>
2024-07-24 00:50:58 -0300

    fixing static bug

--- setup/static/js/dropdownMenu.js
+++
@@ -1,8 +0,0 @@
-const dropdownButton = document.querySelector('#dropdown-button')
-const dropdownList = document.querySelector('.nav-links-container')
-
-function toggle() {
-  dropdownList.classList.toggle('hidden')
-}
-
-dropdownButton.addEventListener('click', toggle)
