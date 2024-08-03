cdcdf2a58ba79728041b996c2d1161c0d4bc7282
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-03 14:43:21 -0300

    Adding upload and download functionality

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import uploadPage
+from .views import uploadPage, ListImages
 
 urlpatterns = [
-    path('upload/', uploadPage, name="upload")
+    path('upload/', uploadPage, name="upload"),
+    path('list/images', ListImages.as_view(), name="listImages"),
 ]
--- pictures/views.py
+++ pictures/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .forms import ImageForm
 from .models import Image
 
+from django.urls import reverse_lazy
+
+from django.views.generic.list import ListView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from braces.views import GroupRequiredMixin
+
 # Create your views here.
 def uploadPage(request):
  if request.method == "POST":
@@ -11,3 +18,9 @@ def uploadPage(request):
  form = ImageForm()
  img = Image.objects.all()
  return render(request, 'uploadPage.html', {'img':img, 'form':form})
+
+class ListImages(GroupRequiredMixin,LoginRequiredMixin,ListView):
+        login_url = reverse_lazy('login')
+        group_required = [u"Admin" u"EquipMember"]
+        model = Image
+        template_name = "listImage.html"
