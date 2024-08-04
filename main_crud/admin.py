b4fc945d13f9067b81a4c143aaaa285bbb8248ab
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-04 15:03:31 -0300

    Adding some changes in user order list

--- main_crud/admin.py
+++ main_crud/admin.py
@@ -12,3 +12,4 @@ class ProfileAdmin(admin.ModelAdmin):
  list_display = ['user','scheduled', 'time','addressOne', 'addressTwo','zipCode','city', 'state','services', 'order_status', 'date']
  list_filter = (("date", DateRangeFilterBuilder()), 'order_status','state')
  search_fields = ['addressOne', 'addressTwo','zipCode','city', 'state', 'services']
+ ordering = ('-date',)
