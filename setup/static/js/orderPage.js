757706917c491b92cc0040a32655679da92eaffb
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-04 10:20:31 -0300

    fixing js bug on order page

--- setup/static/js/orderPage.js
+++ setup/static/js/orderPage.js
@@ -1,11 +1,16 @@
-var infoBtn = document.getElementById("view");
+var buttons = document.querySelectorAll('.viewInfo');
+var infoSections = document.querySelectorAll('.listInfo');
 
-infoBtn.addEventListener("click",function(){
-  var info = document.getElementById("listInfo");
-
-  if(info.classList.contains('d-none')){
-    info.classList.remove('d-none')
-  }else{
-    info.classList.toggle('d-none')
-  }
+// Loop through each button
+buttons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+        var info = infoSections[index];
+        if(info.classList.contains('d-none')){
+            info.classList.remove('d-none')
+            button.textContent = 'Hide';
+        } else {
+            info.classList.toggle('d-none')
+            button.textContent = 'View';
+        }
+    });
 });
