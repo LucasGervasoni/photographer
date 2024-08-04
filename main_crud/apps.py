d2524242b6db12f0a493019ac87341ec1790627f
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-04 13:41:57 -0300

    changing app name in django admin

--- main_crud/apps.py
+++ main_crud/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MainCrudConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_crud'
+    verbose_name = 'Orders Dashboard'
