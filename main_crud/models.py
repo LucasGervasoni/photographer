9b94bdf78f8c5e09a9d2154a8ab266802aef6729
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-13 20:14:38 -0300

    makemigrations

--- main_crud/models.py
+++ main_crud/models.py
@@ -23,11 +23,11 @@ class Order(models.Model):
   
   appointment_team_members = models.CharField(max_length=300,verbose_name="Appointment Member")
   customer = models.CharField(max_length=200, verbose_name="Customer")
-  appointment_date = models.DateTimeField(verbose_name="Scheduled", null=True)
+  appointment_date = models.CharField(verbose_name="Scheduled", null=True)
   address = models.CharField(max_length=200, verbose_name="Address")
   appointment_items = models.CharField(max_length=200, verbose_name="Services")
   order_status = models.CharField(choices=status, null=False, blank=False, default="Not Uploaded", verbose_name="Status")
-  order_created_at = models.DateTimeField(auto_created=True, editable=False, verbose_name="Created At")
+  order_created_at = models.CharField(editable=False, verbose_name="Created At")
 
   def __str__(self):
     return "Photographer: {}, Order: {}".format(self.appointment_team_members, self.address)
