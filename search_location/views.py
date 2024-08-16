d6b1bbff138265c7cb8e500ac0670875c402656c
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-16 09:17:33 -0300

    Adding google API to te project

--- search_location/views.py
+++ search_location/views.py
@@ -5,18 +5,18 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.contrib.auth.models import Group
 from geopy.distance import geodesic
-# Imports the `geodesic` function from the `geopy` library, which is used to calculate the distance between two geographic points.
-from geopy.geocoders import Nominatim
-# Imports the `Nominatim` geocoder from the `geopy` library, used to convert addresses into geographic coordinates.
-from geopy.exc import GeocoderTimedOut
-# Imports the `GeocoderTimedOut` exception from the `geopy` library, used to handle timeout errors during geocoding.
+from geopy.geocoders import GoogleV3
+from geopy.exc import GeocoderTimedOut, GeocoderQuotaExceeded, GeocoderInsufficientPrivileges
 from functools import lru_cache
-# Imports the `lru_cache` decorator from the `functools` library, which is used to cache the results of functions, in this case, the geocoding.
 import time
-# Imports the `time` module, which is used to add delays (sleep) between geocoding attempts, helping to avoid overloading the service.
+import os
 
+# Obtenha a chave da variável de ambiente
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
-# Function with retry e caching
+# Configure o geolocator com sua chave de API do Google
+geolocator = GoogleV3(api_key=GOOGLE_API_KEY, timeout=10)
+
 @lru_cache(maxsize=1000)
 def do_geocode(address, geolocator, attempt=1, max_attempts=3):
     try:
@@ -31,48 +31,39 @@ def do_geocode(address, geolocator, attempt=1, max_attempts=3):
 def search_nearby_users(request):
     if request.method == 'GET':
         address = request.GET.get('address')
-        # Handle max_distance by providing a default value if it's not set or is invalid
         max_distance_str = request.GET.get('max_distance', '10').strip()
-        if max_distance_str:
-            try:
-                max_distance = float(max_distance_str)
-            except ValueError:
-                max_distance = 10.0  # Default to 10 km if conversion fails
-        else:
-            max_distance = 10.0  # Default to 10 km if max_distance is empty
+        max_distance = float(max_distance_str) if max_distance_str else 10.0
 
-        # Configure the geolocator with a longer timeout time
-        geolocator = Nominatim(user_agent="geoapi", timeout=10)
+        try:
+            location = do_geocode(address, geolocator)
+            if location:
+                user_location = (location.latitude, location.longitude)
+                photographer_group = Group.objects.get(name='Photographer')
+                profiles = Profile.objects.filter(username__groups=photographer_group)
+                nearby_users = []
 
-        # Use the geocoding function with retry and caching
-        location = do_geocode(address, geolocator)
+                for profile in profiles:
+                    profile_location = do_geocode(profile.address, geolocator)
+                    if profile_location:
+                        profile_coords = (profile_location.latitude, profile_location.longitude)
+                        distance = geodesic(user_location, profile_coords).km
+                        if distance <= max_distance:
+                            nearby_users.append((profile, distance))
+
+                nearby_users.sort(key=lambda x: x[1])  # Sort by distance
+                paginator = Paginator(nearby_users, 10)
+                page_number = request.GET.get('page')
+                page_obj = paginator.get_page(page_number)
+
+                return render(request, 'searchUsers.html', {'page_obj': page_obj, 'address': address})
+
+            return render(request, 'searchUsers.html', {'error': 'Invalid address or geocoding service timed out. Please try again later.'})
         
-        if location:
-            user_location = (location.latitude, location.longitude)
-            # Get the group for photographers
-            photographer_group = Group.objects.get(name='Photographer')
-            
-            profiles = Profile.objects.filter(username__groups=photographer_group)
-            nearby_users = []
-
-            for profile in profiles:
-                profile_location = do_geocode(profile.address, geolocator)
-                if profile_location:
-                    profile_coords = (profile_location.latitude, profile_location.longitude)
-                    distance = geodesic(user_location, profile_coords).km
-                    if distance <= max_distance:
-                        nearby_users.append((profile, distance))
-
-            # Sort users by proximity
-            nearby_users.sort(key=lambda x: x[1])  # Sort by distance
-            
-            # Add pagination, 10 users per page
-            paginator = Paginator(nearby_users, 10)
-            page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
-
-            return render(request, 'searchUsers.html', {'page_obj': page_obj,'address': address})
-        
-        return render(request, 'searchUsers.html', {'error': 'Invalid address or geocoding service timed out. Please try again later.'})
+        except GeocoderQuotaExceeded:
+            return render(request, 'searchUsers.html', {'error': 'Geocoding service quota exceeded. Please try again later.'})
+        except GeocoderInsufficientPrivileges:
+            return render(request, 'searchUsers.html', {'error': 'Insufficient privileges to use the geocoding service. Please contact support.'})
+        except Exception as e:
+            return render(request, 'searchUsers.html', {'error': f'An unexpected error occurred: {str(e)}'})
 
     return render(request, 'searchUsers.html')
