b17efee754c9a1142809aae63bd167d0d8ffc41d
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-20 10:50:17 -0300

    fixing bucket bugs

--- setup/settings.py
+++ setup/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # AWS S3 configuration for storing static, media, and converted images files
 AWS_ACCESS_KEY_ID = 'AKIAQMEY55YT5HZ5WEKW'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'spotlight-prod-us-west-1-static-media'
 AWS_S3_REGION_NAME = 'us-west-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
