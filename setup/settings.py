c403a82563d9d69a603e88461c145bd7d8e47503
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-23 19:24:14 -0300

    Adding aws database

--- setup/settings.py
+++ setup/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spotlightrds',
-        'USER': 'spotlightmanager',
-        'PASSWORD':  '6p6BKPX1MOqWm2Bk3Tao',
-        'HOST': 'spotlightrds.cj0qe448e9p0.us-west-1.rds.amazonaws.com',
+        'NAME': 'spotlightdatabase',
+        'USER': 'manager',
+        'PASSWORD': 'l4TJsvRQD6wOWjCr1grd',
+        'HOST': 'spotlightdatabase.cj0qe448e9p0.us-west-1.rds.amazonaws.com',
         'PORT': 5432,
         'CONN_MAX_AGE': 600,
         'OPTIONS': {
