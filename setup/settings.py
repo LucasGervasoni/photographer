324b88786b90f10dd42043db72e7ab270b22c9d9
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-23 20:50:24 -0300

    Adding a new aws database

--- setup/settings.py
+++ setup/settings.py
@@ -89,20 +89,20 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spotlightdatabase',
-        'USER': 'manager',
-        'PASSWORD': 'l4TJsvRQD6wOWjCr1grd',
-        'HOST': 'spotlightdatabase.cj0qe448e9p0.us-west-1.rds.amazonaws.com',
-        'PORT': 5432,
-        'CONN_MAX_AGE': 600,
-        'OPTIONS': {
-            'connect_timeout': 60,
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dbspotlight',
+#         'USER': 'manager',
+#         'PASSWORD': '6Nv2suxNNcLD3o4xXnJD',
+#         'HOST': 'dbspotlight.cj0qe448e9p0.us-west-1.rds.amazonaws.com',
+#         'PORT': 5432,
+#         'CONN_MAX_AGE': 600,
+#         'OPTIONS': {
+#             'connect_timeout': 60,
+#         },
+#     }
+# }
 
 
 # Password validation
