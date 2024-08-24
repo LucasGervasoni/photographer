8a3984f21029a86ec3bdf1af64f2fb657b352085
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-24 18:49:04 -0300

    Fixing bug on log page

--- setup/settings.py
+++ setup/settings.py
@@ -80,29 +80,29 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'spotlightdatabase',
-#         'USER': 'manager',
-#         'PASSWORD': 'l4TJsvRQD6wOWjCr1grd',
-#         'HOST': 'spotlightdatabase.cj0qe448e9p0.us-west-1.rds.amazonaws.com',
-#         'PORT': 5432,
-#         'CONN_MAX_AGE': 600,
-#         'OPTIONS': {
-#             'connect_timeout': 60,
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spotlightdatabase',
+        'USER': 'manager',
+        'PASSWORD': 'l4TJsvRQD6wOWjCr1grd',
+        'HOST': 'spotlightdatabase.cj0qe448e9p0.us-west-1.rds.amazonaws.com',
+        'PORT': 5432,
+        'CONN_MAX_AGE': 600,
+        'OPTIONS': {
+            'connect_timeout': 60,
+        },
+    }
+}
 
 
 # Password validation
