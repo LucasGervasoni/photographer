26490822102c306897ea3d36d8e93e9bd2516981
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-25 13:07:34 -0300

    fixing bugs and adding new styles

--- apps/main_crud/forms.py
+++ apps/main_crud/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from apps.main_crud.models import Order
+from apps.pictures.models import OrderImageGroup
 
 class OrderForm(forms.ModelForm):
+    
+    appointment_items = forms.MultipleChoiceField(
+        choices=OrderImageGroup.select_services,
+        widget=forms.CheckboxSelectMultiple,
+        required=True,
+        label="Services"
+    )
+     
     class Meta:
         model = Order
         fields = ['appointment_team_members', 'customer', 'appointment_date', 'address', 'appointment_items', 'order_status']
