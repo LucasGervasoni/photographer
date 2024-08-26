2bbebc1be0ee57138186785581926ca10a441f31
Lucas Gervasoni <jaxellucas@gmail.com>
2024-08-26 20:24:26 -0300

    adding new scan url logic

--- apps/main_crud/views.py
+++ apps/main_crud/views.py
@@ -4,12 +4,11 @@ from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from apps.main_crud.models import Order
 from apps.main_crud.forms import OrderForm
 from apps.pictures.models import OrderImageGroup
-from django.db.models import OuterRef, Subquery 
+from django.db.models import OuterRef, Subquery, Q, Count, Case, When, Value, CharField
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from braces.views import GroupRequiredMixin
 from django.utils.dateparse import parse_date
-from django.db.models import Q
 
 #Create
 class OrderCreateView(GroupRequiredMixin,LoginRequiredMixin,CreateView):
@@ -44,13 +43,35 @@ class AdminListOrders(GroupRequiredMixin,LoginRequiredMixin,ListView):
                  # Construct the full name of the user
                 full_name = f"{user.first_name} {user.last_name}"
                 
-                # Annotate each Order with the latest 3d scan URL from OrderImageGroup
-                latest_scan_url = OrderImageGroup.objects.filter(
+                # Subquery to get the count of scan URLs for each order
+                scan_url_count_subquery = OrderImageGroup.objects.filter(
                     order=OuterRef('pk'),
-                    services__icontains='3d scan'
+                    services__icontains="3d scan",
+                    scan_url__isnull=False
+                ).values('order').annotate(count=Count('scan_url')).values('count')[:1]
+
+                # Subquery to get the first scan URL
+                first_scan_url_subquery = OrderImageGroup.objects.filter(
+                    order=OuterRef('pk'),
+                    services__icontains="3d scan",
+                    scan_url__isnull=False
+                ).order_by('created_at').values('scan_url')[:1]
+
+                # Subquery to get the most recent scan URL
+                recent_scan_url_subquery = OrderImageGroup.objects.filter(
+                    order=OuterRef('pk'),
+                    services__icontains="3d scan",
+                    scan_url__isnull=False
                 ).order_by('-created_at').values('scan_url')[:1]
 
-                queryset = queryset.annotate(latest_scan_url=Subquery(latest_scan_url))
+                queryset = Order.objects.annotate(
+                    scan_url_count=Subquery(scan_url_count_subquery),
+                    latest_scan_url=Case(
+                        When(scan_url_count__gt=1, then=Subquery(recent_scan_url_subquery)),
+                        default=Subquery(first_scan_url_subquery),
+                        output_field=CharField(),
+                    )
+                )
                 
 
                 # Check if the user is not an editor or superuser
@@ -137,13 +158,36 @@ class UserPageOrders(LoginRequiredMixin, ListView):
         # Construct the full name of the user
         full_name = f"{user.first_name} {user.last_name}"
 
-        # Annotate each Order with the latest 3d scan URL from OrderImageGroup
-        latest_scan_url = OrderImageGroup.objects.filter(
+        # Subquery to get the count of scan URLs for each order
+        scan_url_count_subquery = OrderImageGroup.objects.filter(
             order=OuterRef('pk'),
-            services__icontains='3d scan'
+            services__icontains="3d scan",
+            scan_url__isnull=False
+        ).values('order').annotate(count=Count('scan_url')).values('count')[:1]
+
+        # Subquery to get the first scan URL
+        first_scan_url_subquery = OrderImageGroup.objects.filter(
+            order=OuterRef('pk'),
+            services__icontains="3d scan",
+            scan_url__isnull=False
+        ).order_by('created_at').values('scan_url')[:1]
+
+        # Subquery to get the most recent scan URL
+        recent_scan_url_subquery = OrderImageGroup.objects.filter(
+            order=OuterRef('pk'),
+            services__icontains="3d scan",
+            scan_url__isnull=False
         ).order_by('-created_at').values('scan_url')[:1]
 
-        queryset = queryset.annotate(latest_scan_url=Subquery(latest_scan_url))
+        queryset = Order.objects.annotate(
+            scan_url_count=Subquery(scan_url_count_subquery),
+            latest_scan_url=Case(
+                When(scan_url_count__gt=1, then=Subquery(recent_scan_url_subquery)),
+                default=Subquery(first_scan_url_subquery),
+                output_field=CharField(),
+            )
+        )
+
 
         # Filter by user, status, search, and date
         if not user.is_superuser and not user.groups.filter(name='Editor').exists():
